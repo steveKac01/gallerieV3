465f606cf1bb6d5cf1a25b81d23628a414aef314
Steve Kaci <kaci.steve@k-net.fr>
2022-01-23 19:44:20 +0100

    the end

--- js/index.js
+++ js/index.js
@@ -1,42 +1,71 @@
 // Can be edited
-let startImg = 0;
-let maxImg = 8;
-let pathImg = "Img/0";
-let positionStartDiv = 0;
+let startImg = 0; //Start from # images
+let maxImg = 8; //Maximum images
+let autoPlay = false;
+let timer = 2000; //Timer in ms
 
 //Do not touch
-//Nbr de pixel a se deplacer
-let moveWidth = document.querySelector(".containerImg > img").clientWidth; //
-let positionDiv = positionStartDiv;
+let positionDiv = 0;
+let idInterval;
+maxImg--;
+
+let moveWidth = document.querySelector(".containerImg > img").clientWidth;
 
 //Grab
 let btnArray = document.querySelectorAll(".btn-container div");
+let iconePlayStop = document.querySelector(
+  ".btn-container div:nth-of-type(2) > i"
+);
 let divToMove = document.querySelector(".containerImg");
 
 //Events
 for (let i = 0; i < btnArray.length; i++) {
   btnArray[i].addEventListener("click", function () {
-    let sens = 0;
-    if (i == 0) {
-      //Previous btn
-      sens = -1;
-    } else {
-      //Next btn
-      sens = 1;
+    switch (
+      i //Previous
+    ) {
+      case 0:
+        MoveImage(-1);
+        break;
+      case 1: //Play auto
+        switchAutoMode();
+        break;
+      case 2: //Next
+        MoveImage(1);
+        break;
     }
-
-    console.log("je bouge");
-    positionDiv = positionDiv - moveWidth * sens;
-    if (positionDiv > 0) {
-      positionDiv = -800 * (maxImg - 1);
-    }
-    if (positionDiv < (maxImg - 1) * 800 * -1) {
-      positionDiv = 0;
-    }
-    divToMove.style.transform = "translateX(" + positionDiv + "px)";
-    console.log(positionDiv);
   });
 }
 
-//Functions
-function next() {}
+// Move the div .containerImg
+function MoveImage(direction = 1) {
+  positionDiv = positionDiv - moveWidth * direction;
+  if (positionDiv > 0) {
+    positionDiv = -800 * maxImg;
+  }
+  if (positionDiv < maxImg * 800 * -1) {
+    positionDiv = 0;
+  }
+  divToMove.style.transform = "translateX(" + positionDiv + "px)";
+}
+
+//Switch auto play or stop mode.
+function switchAutoMode() {
+  if (!autoPlay) {
+    autoPlay = true;
+    iconePlayStop.classList.replace("fa-play", "fa-stop");
+    idInterval = setInterval(MoveImage, timer);
+    ShowHideBtns();
+  } else {
+    autoPlay = false;
+    iconePlayStop.classList.replace("fa-stop", "fa-play");
+    clearInterval(idInterval);
+    ShowHideBtns();
+  }
+}
+
+//Hide or display the next and previous buttons.
+function ShowHideBtns() {
+  btnArray[0].classList.toggle("hide");
+  btnArray[2].classList.toggle("hide");
+}
